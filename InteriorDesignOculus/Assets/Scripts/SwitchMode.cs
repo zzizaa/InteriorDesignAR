b379e03947a0e25363ac64ec2d011e570d92cc3e
zzizaa <gabriele.ziza@gmail.com>
2023-06-08 16:00:35 +0200

    Added SwitchMode
    -added Pose detection
    -Change Hand Colour when switching Mode

---
+++ InteriorDesignOculus/Assets/Scripts/SwitchMode.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMode : MonoBehaviour
+{
+   public bool editingMode;
+   [SerializeField] private GameObject _leftHand;
+   [SerializeField] private GameObject _rightHand;
+   [SerializeField] private Material _editMaterial;
+   [SerializeField] private Material _baseMaterial;
+   
+   public void ChangeMode()
+   {
+      editingMode = !editingMode;
+      ChangeMaterial();
+   }
+
+   private void ChangeMaterial()
+   {
+      if (editingMode)
+      {
+         _leftHand.GetComponent<SkinnedMeshRenderer>().material = new Material(_editMaterial);
+         _rightHand.GetComponent<SkinnedMeshRenderer>().material = new Material(_editMaterial);
+      }
+      else
+      {
+         _leftHand.GetComponent<SkinnedMeshRenderer>().material = new Material(_baseMaterial);
+         _rightHand.GetComponent<SkinnedMeshRenderer>().material = new Material(_baseMaterial);
+      }
+   }
+}
